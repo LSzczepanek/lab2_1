bbc0c890638fef74c5d049289c48cc3c3683dca4
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2016-12-17 17:05:42 +0100

    Adding first testCase where we check element is found in sequence

--- src/test/java/lab2_1/TestSearchMethod.java
+++ src/test/java/lab2_1/TestSearchMethod.java
@@ -1,5 +1,20 @@
 package lab2_1;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+
 public class TestSearchMethod {
 
+	
+	@Test
+	public void testIsElementFoundInSequence(){
+		int key = 4;
+		int[] seq = {4};
+		
+		assertTrue(BinarySearch.search(key, seq).isFound());
+	}
+	
 }
