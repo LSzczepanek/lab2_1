f38cff1055833508cca93a78b6aebe1edab4ceb0
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2017-01-14 15:08:28 +0100

    Adding testCase longer in seq not found element

--- src/test/java/lab2_1/TestSearchMethod.java
+++ src/test/java/lab2_1/TestSearchMethod.java
@@ -11,130 +11,136 @@ import edu.iis.mto.bsearch.SearchResult;
 
 public class TestSearchMethod {
 
-	
 	@Test
-	public void testElementExistInSingleSequence(){
+	public void testElementExistInSingleSequence() {
 		int key = 4;
-		int[] seq = {4};
+		int[] seq = { 4 };
 		SearchResult result = BinarySearch.search(key, seq);
-		
+
 		assertThat(result.isFound(), is(true));
 		assertThat(seq[result.getPosition()], is(equalTo(key)));
 	}
-	
-	
+
 	@Test
-	public void testElementNotExistInSingleSequence(){
+	public void testElementNotExistInSingleSequence() {
 		int key = 7;
-		int[] seq = {4};
+		int[] seq = { 4 };
 		SearchResult result = BinarySearch.search(key, seq);
-		
+
 		assertThat(result.isFound(), is(false));
 		assertThat(result.getPosition(), is(equalTo(-1)));
 	}
-	
+
 	@Test
-	public void testElementIsFirstInSequence(){
+	public void testElementIsFirstInSequence() {
 		int key = 5;
-		int[] seq = {5,6,7};
+		int[] seq = { 5, 6, 7 };
 		SearchResult result = BinarySearch.search(key, seq);
-		
+
 		assertThat(result.getPosition(), is(equalTo(0)));
 	}
-	
-	
+
 	@Test
-	public void testElementIsLastInSequence(){
+	public void testElementIsLastInSequence() {
 		int key = 7;
-		int[] seq = {5,6,7};
+		int[] seq = { 5, 6, 7 };
 		SearchResult result = BinarySearch.search(key, seq);
-		
-		assertThat(result.getPosition(), is(equalTo(seq.length-1)));
+
+		assertThat(result.getPosition(), is(equalTo(seq.length - 1)));
 	}
-	
+
 	@Test
-	public void testElementIsMiddleInSequence(){
+	public void testElementIsMiddleInSequence() {
 		int key = 6;
-		int[] seq = {5,6,7};
+		int[] seq = { 5, 6, 7 };
 		SearchResult result = BinarySearch.search(key, seq);
-		
+
 		assertThat(result.getPosition(), is(equalTo(1)));
 	}
-	
-	
+
 	@Test
-	public void testElemenetExistInLongerSequence(){
+	public void testElemenetExistInLongerSequence() {
 		int key = 4;
-		int[] seq = {4,5,6,7};
+		int[] seq = { 4, 5, 6, 7 };
 		SearchResult result = BinarySearch.search(key, seq);
-		
+
 		assertThat(result.isFound(), is(true));
 		assertThat(seq[result.getPosition()], is(equalTo(key)));
 	}
-	
+
+	@Test
+	public void testElemenetIsNotExistInLongerSequence() {
+		int key = 2;
+		int[] seq = { 4, 5, 6, 7 };
+		SearchResult result = BinarySearch.search(key, seq);
+
+		assertThat(result.isFound(), is(false));
+		assertThat(result.getPosition(), is(equalTo(-1)));
+	}
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testWhenSequenceIsEmpty(){
+	public void testWhenSequenceIsEmpty() {
 		int key = 4;
 		int[] seq = {};
 
 		BinarySearch.search(key, seq);
 	}
-	
+
 	@Test
-	public void testWhenKeyValueIsZero(){
+	public void testWhenKeyValueIsZero() {
 		int key = 0;
-		int[] seq = {0,5,6,7};
+		int[] seq = { 0, 5, 6, 7 };
 
 		SearchResult result = BinarySearch.search(key, seq);
 
 		assertThat(result.isFound(), is(true));
 		assertThat(seq[result.getPosition()], is(equalTo(key)));
 	}
-	
+
 	@Test
-	public void testWhenKeyIsNegative(){
+	public void testWhenKeyIsNegative() {
 		int key = -5;
-		int[] seq = {4,-5,6,7};
+		int[] seq = { 4, -5, 6, 7 };
 		SearchResult result = BinarySearch.search(key, seq);
 
 		assertThat(result.isFound(), is(true));
 		assertThat(seq[result.getPosition()], is(equalTo(key)));
 	}
-	
+
 	@Test
-	public void testWhenSequeanceIsNegative(){
+	public void testWhenSequeanceIsNegative() {
 		int key = -4;
-		int[] seq = {-6,-5,-4,-3,-2};
+		int[] seq = { -6, -5, -4, -3, -2 };
 		SearchResult result = BinarySearch.search(key, seq);
 
 		assertThat(result.isFound(), is(true));
 		assertThat(seq[result.getPosition()], is(equalTo(key)));
 	}
-	
+
 	@Test
-	public void testWhenThereIsNoSequence(){
+	public void testWhenThereIsNoSequence() {
 		int key = 3;
-		int[] seq = {1,7,3,2,12};
+		int[] seq = { 1, 7, 3, 2, 12 };
 		SearchResult result = BinarySearch.search(key, seq);
 
 		assertThat(result.isFound(), is(false));
 		assertThat(result.getPosition(), is(equalTo(-1)));
 	}
-	
+
 	@Test
-	public void testWhenKeyValueIsOverfull(){
+	public void testWhenKeyValueIsOverfull() {
 		int key = Integer.MAX_VALUE + 1;
-		int[] seq = {4,5,6,7};
+		int[] seq = { 4, 5, 6, 7 };
 		SearchResult result = BinarySearch.search(key, seq);
 
 		assertThat(result.isFound(), is(false));
 		assertThat(result.getPosition(), is(equalTo(-1)));
 	}
-	
+
 	@Test
-	public void testWhenInSeqIsOverfullValue(){
+	public void testWhenInSeqIsOverfullValue() {
 		int key = 5;
-		int[] seq = {4,5,6,7, Integer.MAX_VALUE+1};
+		int[] seq = { 4, 5, 6, 7, Integer.MAX_VALUE + 1 };
 		SearchResult result = BinarySearch.search(key, seq);
 
 		assertThat(result.isFound(), is(true));
